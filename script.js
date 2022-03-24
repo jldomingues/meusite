016e87d87072adfd0c0b624fafc2b9b8a8d051b2
jldomingues <97459319+jldomingues@users.noreply.github.com>
2022-03-24 06:42:11 -0300

    Update script.js
    Novas funcionalidades

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ function img(){
         plocal.innerHTML=(`<img src="imgs/print.png"><br>
       <div onclick="history.go(0)">  Minimizar</div>`)
       }
+function img2(){
+        document.getElementById('img2').style.display="none"
+        var plocal=document.getElementById('local2')
+        plocal.innerHTML=(`<img src="imgs/print2.png"><br>
+      <div onclick="history.go(0)">  Minimizar</div>`)
+      }
