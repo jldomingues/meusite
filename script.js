0039876aad2315ffff83903ef3b355354facf6fd
lucas.domingues16@gmail.com <lucas.domingues16@gmail.com>
2022-04-10 16:52:29 -0300

    new important update

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+function projeto(){
+  document.getElementById('encolher').style.display="none"
+        var plocal=document.getElementById('projeto')
+        plocal.innerHTML=(`<div class="nome"><img src="imgs/project.png" width="40px">Projetos<br><h2 onclick="projeto()">Clique no ícone de minimizar para encolher <img src="imgs/encolher.png" width="20px" onclick="history.go(0)"></h2><br><div id="fonte"><p> Meu Lucro é uma aplicação desenvolvida em Java Script<br>ela foi desenvolvida para calcular o juros de um produto em diferentes faixas de preços<br>começando em 40% e decaindo em outras faixas de preço,<br>outra funcionalidade legal, é poder salvar ou imprimir seus calculos.</p><div id="img" onclick="img()"><img src="imgs/print.png" width="250px" title="Clique na imagem para aumentar"></div><div id="local"></div><br><h4><a href="https://jldomingues.github.io/meulucro/">Acessar Aplicação</a></h4><p> CatFit é uma aplicação desenvolvida em Javascript<br>
+    com intuito de calcular o imc do usuário<br>De maneira cômica e divertida. <br><div id="img2" onclick="img2()"> <img src="imgs/print2.png" width="250px"></div><div id="local2"></div><br>
+    <h4><a href="https://jldomingues.github.io/catfit/"> Acessar Aplicação</h4></a><p> Copa do Mundo 2022 é uma aplicacão em Javascript com o intuito de organizar <br>os grupos da copa do mundo<br><div id="img3" onclick="img3()"> <img src="imgs/print3.png" width="250px"></div><div id="local3"></div><br><h4><a href="https://jldomingues.github.io/copadomundo2022/"> Acessar Aplicação</h4></a></div><br>`)
+    
+    
+      }
+function video(){
+  
+        var plocal=document.getElementById('video')
+       window.alert('Conteúdo em desenvolvimento')
+
+}
+
+
+
 function img(){
         document.getElementById('img').style.display="none"
         var plocal=document.getElementById('local')
